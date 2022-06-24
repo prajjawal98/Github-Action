1c9797cc5eb3eab321c63e5e9d8cd637ef4b6f86
prajjawal98 <prajjawal.kansal@knoldus.com>
2022-06-25 01:03:09 +0530

    new code

---
+++ src/test/java/com/BaseClass.java
@@ -0,0 +1,34 @@
+package com;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.concurrent.TimeUnit;
+
+public class BaseClass {
+
+    public WebDriver driver;
+    public static ThreadLocal<WebDriver> tdriver = new ThreadLocal<WebDriver>();
+
+    public WebDriver initialize_driver() {
+
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        options.addArguments("--disable-gpu", "--window-size=1920,1200","--ignore-certificate-errors","--disable-extensions","--no-sandbox","--disable-dev-shm-usage");
+
+        driver = new ChromeDriver(options);
+        //
+        //driver = new ChromeDriver();
+        driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        tdriver.set(driver);
+        return getDriver();
+    }
+
+    public static synchronized WebDriver getDriver() {
+        return tdriver.get();
+    }
+}
---
+++ src/test/java/testcase/Test.java
@@ -0,0 +1,52 @@
+package testcase;
+
+import com.BaseClass;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+public class Test extends BaseClass {
+
+    public WebDriver driver;
+
+    @BeforeClass
+    public void setUp() {
+        BaseClass bs= new BaseClass();
+        driver = bs.initialize_driver();
+        driver.get("https://demo.nopcommerce.com/");
+
+    }
+
+    @org.testng.annotations.Test
+    public void loginTest1() throws InterruptedException
+    {
+        driver.findElement(By.linkText("Log in")).click();
+        driver.findElement(By.id("Email")).sendKeys("prajjawalkansal@gmail.com");
+        driver.findElement(By.id("Password")).sendKeys("testcase.Test@123");
+        driver.findElement(By.xpath("//button[@class='button-1 login-button']")).submit();
+        Thread.sleep(3000);
+        Assert.assertEquals(driver.getTitle(), "nopCommerce demo store. Login");
+
+    }
+
+    @org.testng.annotations.Test
+    public void loginTest2() throws InterruptedException {
+        driver.findElement(By.linkText("Log in")).click();
+        driver.findElement(By.id("Email")).sendKeys("avadhsharma566@gmail.com");
+        driver.findElement(By.id("Password")).sendKeys("prajjawal1234");
+        driver.findElement(By.xpath("//button[@class='button-1 login-button']")).submit();
+        Thread.sleep(3000);
+
+        Assert.assertEquals(driver.getTitle(), "nopCommerce demo store. Login");
+    }
+
+
+    @AfterClass
+    public void tearDown()
+    {
+        driver.quit();
+    }
+
+}
